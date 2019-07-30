80eb17fd104749471cd285a8f49286d2f57616eb
Cherukuri.Sarath Babu.X <SarathBabuCherukuri@westfieldgrp.com>
2019-07-29 20:56:48 -0400

    updating to github

---
+++ Summatives/Dvd Manager/DvdManager.Models/Dvd.cs
@@ -0,0 +1,12 @@
+﻿namespace DvdManager.Models
+{
+    public class Dvd
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Director { get; set; }
+        public float Rating { get; set; }
+        
+    }
+}
